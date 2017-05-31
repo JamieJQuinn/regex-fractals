61cbfaeead63934b8e45ee558a1ade0d0fec7062
Jamie Quinn <j.quinn.1@research@gla.ac.uk>
2017-05-31 17:42:04 +0100

    Added some settings

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function drawCell(cell, n, context, color) {
+function drawCell(cell, n, context) {
   var width = n/Math.pow(2, cell.length);
   var pt = cellToPt(cell, n);
   context.fillRect(pt[0], pt[1], width, width);
@@ -27,19 +27,19 @@ function getColourValue(i) {
   }
 }
 
-function applyRegToCell(reg, n, cell, context) {
+function applyRegToCell(reg, cell, context, colour) {
+  var n = context.canvas.width;
   if(Math.pow(2, cell.length) >= n) {
     return;
   }
   // if the regex checks out
   if(reg.test(cell)) {
-    var color = '#000000';
     //color = '#' + getColourValue(Math.round(255/cell.length)) + '0000';
-    context.fillStyle = color;
-    drawCell(cell, n, context, color);
+    context.fillStyle = colour;
+    drawCell(cell, n, context);
   } else {
     for(var i=1; i<=4; ++i) {
-      applyRegToCell(reg, n, cell + i.toString(), context);
+      applyRegToCell(reg, cell + i.toString(), context, colour);
     }
   }
 }
@@ -49,44 +49,50 @@ function getContext() {
   return canvas.getContext("2d");
 }
 
-function draw(regex_str, ctx) {
+function draw(regex_str, drawing_ctx, colour, bg_colour) {
   var ctx = getContext();
   var size = ctx.canvas.width;
-  var pow2size = Math.pow(2, Math.ceil(Math.log2(size)));
-
-  var drawing_canvas = document.createElement('canvas');
-  var drawing_ctx = drawing_canvas.getContext("2d");
-  drawing_canvas.width = pow2size;
-  drawing_canvas.height = pow2size;
+  var pow2size = drawing_ctx.canvas.width;
 
   // Draw background
-  drawing_ctx.fillStyle = '#fff';
-  drawing_ctx.fillRect(0, 0, size, size);
+  drawing_ctx.fillStyle = bg_colour;
+  drawing_ctx.fillRect(0, 0, pow2size, pow2size);
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = colour;
 
   var regex = new RegExp(regex_str);
 
   // Draw to power of 2 sized canvas
-  applyRegToCell(regex, pow2size, '', drawing_ctx);
+  applyRegToCell(regex, '', drawing_ctx, colour);
 
   // Draw that canvas to actual screen
-  ctx.drawImage(drawing_canvas, 0, 0, size, size);
+  ctx.drawImage(drawing_ctx.canvas, 0, 0, size, size);
 }
 
-var regexes = ["1", "21", "42", "13|31|24|42", "13|31|24|42", "13|31", "4[^4][^4]2", "1[124]|2[14]|4[12]|31", "[13][24]|[24][13]", "12", "12|21", "12|21|34|43", "[34]*2"]
-var regex_str = regexes[Math.floor(Math.random()*regexes.length)];
 
 function main() {
+  var regexes = ["1", "21", "42", "13|31|24|42", "13|31|24|42", "13|31", "4[^4][^4]2", "1[124]|2[14]|4[12]|31", "[13][24]|[24][13]", "12", "12|21", "12|21|34|43", "[34]+2"]
+  var regex_str = regexes[Math.floor(Math.random()*regexes.length)];
+
   var ctx = getContext();
   var size = Math.min(window.innerWidth, window.innerHeight);
   ctx.canvas.width = size;
   ctx.canvas.height = size;
 
-  ctx.canvas.addEventListener("click", function(e) {
-    regex_str = window.prompt("Enter regex", regex_str);
-    draw(regex_str);
-  })
+  var drawing_canvas = document.createElement('canvas');
+  var drawing_ctx = drawing_canvas.getContext("2d");
+  var pow2size = Math.pow(2, Math.ceil(Math.log2(size)));
+  drawing_canvas.width = pow2size;
+  drawing_canvas.height = pow2size;
 
-  draw(regex_str);
+  var colour = "#53828d";
+  var bg_colour = "#a19a9a";
+
+  draw(regex_str, drawing_ctx, colour, bg_colour);
+
+  var settings = QuickSettings.create(5, 5, "Regex Fractal Settings");
+  settings.addText("Regex", regex_str, function(txt) {regex_str = txt; draw(regex_str, drawing_ctx, colour, bg_colour)});
+  settings.addColor("Background Colour", colour, function(c) {colour = c; draw(regex_str, drawing_ctx, colour, bg_colour)});
+  settings.addColor("Colour", colour, function(c) {bg_colour = c; draw(regex_str, drawing_ctx, colour, bg_colour)});
 }
 
 main();
